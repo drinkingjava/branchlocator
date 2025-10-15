07d7d542cc6fb0acdec4e0b77aa957bf96e0ff09
Algerchen <algerchen@tsn.systems>
2025-10-15 02:35:12 +0200

    Added autocomplete search for locations. Also, fixed issue where autocomplete results don't show by upping its z-index

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -20,10 +20,11 @@ var geojsonFeature = {
 export default function Map() {
   const mapRef = useRef();
   const userMarkerRef = useRef();
-    const [userGeoLocation, setUserGeoLocation] = useState({
-      latitude: -33.8921342,
-      longitude: 18.5597664013195,
-    });
+  const autoCompleteRef = useRef();
+  const [userGeoLocation, setUserGeoLocation] = useState({
+    latitude: -33.8921342,
+    longitude: 18.5597664013195,
+  });
 
   // const [userGeoLocation, setUserPosition] = useState({latitude: 0, longitude: 0});
 
@@ -69,7 +70,7 @@ export default function Map() {
   // );
   // console.log(nearbyMarkers)
 
-  // const location = useGeolocation(); // prompt user location or autoset geolocation 
+  // const location = useGeolocation(); // prompt user location or autoset geolocation
 
   // Create Map and set view to default user position(0,0).
   useEffect(() => {
@@ -116,7 +117,7 @@ export default function Map() {
     //     { latitude, longitude },
     //   ]);
     // });
-  }, [nearbyMarkers,userGeoLocation.latitude, userGeoLocation.longitude]);
+  }, [nearbyMarkers, userGeoLocation.latitude, userGeoLocation.longitude]);
 
   useEffect(() => {
     // setUserPosition({ ...userPosition });
@@ -129,16 +130,119 @@ export default function Map() {
     userMarkerRef.current = leaflet
       .marker([userGeoLocation.latitude, userGeoLocation.longitude])
       .addTo(mapRef.current)
-      .bindPopup("Your Current Location").togglePopup()
+      .bindPopup("Your Current Location")
+      .togglePopup();
 
     const el = userMarkerRef.current.getElement();
     if (el) {
       el.style.filter = "hue-rotate(120deg)";
     }
 
-    mapRef.current.setView([userGeoLocation.latitude, userGeoLocation.longitude]);
-
+    mapRef.current.setView([
+      userGeoLocation.latitude,
+      userGeoLocation.longitude,
+    ]);
   }, [userGeoLocation.latitude, userGeoLocation.longitude]);
-  
-  return <div id="map" ref={mapRef}></div>;
+
+
+  useEffect(() => {
+    // minimal configure
+    new Autocomplete("search", {
+      // default selects the first item in
+      // the list of results
+      selectFirst: true,
+
+      // The number of characters entered should start searching
+      howManyCharacters: 2,
+
+      // onSearch
+      onSearch: ({ currentValue }) => {
+        // You can also use static files
+        // const api = '../static/search.json'
+        const api = `https://nominatim.openstreetmap.org/search?format=geojson&limit=5&city=${encodeURI(
+          currentValue
+        )}`;
+        return new Promise((resolve) => {
+          fetch(api)
+            .then((response) => response.json())
+            .then((data) => {
+              resolve(data.features);
+            })
+            .catch((error) => {
+              console.error(error);
+            });
+        });
+      },
+      // nominatim GeoJSON format parse this part turns json into the list of
+      // records that appears when you type.
+      onResults: ({ currentValue, matches, template }) => {
+        const regex = new RegExp(currentValue, "gi");
+
+        // if the result returns 0 we
+        // show the no results element
+        return matches === 0
+          ? template
+          : matches
+              .map((element) => {
+                return `
+          <li class="loupe z-index">
+            <p>
+              ${element.properties.display_name.replace(
+                regex,
+                (str) => `<b>${str}</b>`
+              )}
+            </p>
+          </li> `;
+              })
+              .join("");
+      },
+
+      // we add an action to enter or click
+      onSubmit: ({ object }) => {
+        // remove all layers from the map
+        mapRef.current.eachLayer(function (layer) {
+          if (!!layer.toGeoJSON) {
+            mapRef.current.removeLayer(layer);
+          }
+        });
+
+        const { display_name } = object.properties;
+        const [lng, lat] = object.geometry.coordinates;
+
+        const marker = L.marker([lat, lng], {
+          title: display_name,
+        });
+
+        marker.addTo(mapRef.current).bindPopup(display_name);
+
+        mapRef.current.setView([lat, lng], 8);
+      },
+
+      // get index and data from li element after
+      // hovering over li with the mouse or using
+      // arrow keys ↓ | ↑
+      onSelectedItem: ({ index, element, object }) => {
+        console.log("onSelectedItem:", index, element, object);
+      },
+
+      // the method presents no results element
+      noResults: ({ currentValue, template }) =>
+        template(`<li>No results found: "${currentValue}"</li>`),
+    });
+  })
+  return (
+    <div className="w-full">
+      <div className="auto-search-wrapper">
+        <input
+          type="text"
+          autoComplete="off"
+          id="search"
+          className="full-width"
+          placeholder="enter the city name"
+          ref={autoCompleteRef}
+        />
+      </div>
+      <div id="map" ref={mapRef}></div>
+    </div>
+  );
 }
